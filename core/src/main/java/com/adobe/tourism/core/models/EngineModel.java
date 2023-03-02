d783935089f7fb5d6335da22cd1a772d53e7d8bb
nikhilDigiquad <nikhilt@digiquadsolutions.com>
2023-03-02 17:45:19 +0530

    implementation of  model class for the possible ways for of multifield....

---
+++ core/src/main/java/com/adobe/tourism/core/models/EngineModel.java
@@ -0,0 +1,10 @@
+package com.adobe.tourism.core.models;
+
+import java.util.List;
+import java.util.Map;
+
+public interface EngineModel {
+	
+	    List<Map<String, String>> getEngineDetailsWithMap(); 
+
+}
