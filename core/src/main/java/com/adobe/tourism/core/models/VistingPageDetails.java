6a56ae2fa346956e5f77ea182cc5a6b8bedcbd46
nikhilDigiquad <nikhilt@digiquadsolutions.com>
2023-03-03 17:10:51 +0530

    page details with different annotations

---
+++ core/src/main/java/com/adobe/tourism/core/models/VistingPageDetails.java
@@ -0,0 +1,12 @@
+package com.adobe.tourism.core.models;
+
+
+public interface VistingPageDetails {
+	
+	    String getPageTitle();
+	    String getRequestAttribute();
+	    String getHomePageName();
+	    String getLastModifiedBy();
+	    
+
+}
